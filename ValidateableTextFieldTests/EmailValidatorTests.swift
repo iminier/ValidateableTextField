f002ac373bf42dd37da30871f2a0fb8184c0e057
Ivan Minier <ivanminier@me.com>
2017-07-01 11:17:16 -0700

    EmailValidator Tests Added

--- ValidateableTextFieldTests/EmailValidatorTests.swift
+++ ValidateableTextFieldTests/EmailValidatorTests.swift
@@ -5,6 +5,8 @@
 //  Created by Ivan Minier on 6/30/17.
 //  Copyright © 2017 Alberto Vega Gonzalez. All rights reserved.
 //
+//  Used TestCases found at https://blogs.msdn.microsoft.com/testing123/2009/02/06/email-address-test-cases/
+//
 
 import XCTest
 @testable import ValidateableTextField
@@ -42,34 +44,6 @@ class EmailValidatorTests: XCTestCase {
         XCTAssertTrue(result)
     }
     
-    func testDashAtDomainGenericReturnsFalse() {
-        let email = "-@domain.com"
-        let result = emailValidatorUT.isValidInput(email)
-        
-        XCTAssertFalse(result)
-    }
-    
-    func testDashForDomainReturnsFalse() {
-        let email = "name@-.com"
-        let result = emailValidatorUT.isValidInput(email)
-        
-        XCTAssertFalse(result)
-    }
-    
-    func testEmailWithoutATSymbolReturnsFalse() {
-        let email = "namedomain.com"
-        let result = emailValidatorUT.isValidInput(email)
-        
-        XCTAssertFalse(result)
-    }
-    
-    func testBlankEmailReturnsFalse() {
-        let email = ""
-        let result = emailValidatorUT.isValidInput(email)
-        
-        XCTAssertFalse(result)
-    }
-    //
     func testEmailContainsDotInAddressFieldReturnsTrue() {
         let email = "firstname.lastname@domain.com"
         let result = emailValidatorUT.isValidInput(email)
@@ -103,8 +77,173 @@ class EmailValidatorTests: XCTestCase {
         let result = emailValidatorUT.isValidInput(email)
         
         XCTAssertTrue(result)
-        
     }
     
-}
+    func testNumAtDomainResturnsTrue() {
+        let email = "1234567890@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testEmailAtDomaindashDomainResturnsTrue() {
+        let email = "email@domain-one.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testUnderscoresAsAddressReturnsTrue() {
+        let email = "_______@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testEmailAtTopLevelDomainReturnsTrue() {
+        let email = "email@domain.name"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testDashInAddressReturnsTrue() {
+        let email = "firstname-lastname@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func testDashAtDomainGenericReturnsFalse() {
+        let email = "-@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testDashForDomainReturnsFalse() {
+        let email = "name@-.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testEmailWithoutATSymbolReturnsFalse() {
+        let email = "namedomain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testBlankEmailReturnsFalse() {
+        let email = ""
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
 
+    func testPlainAddressReturnsFalse() {
+        let email = "plainaddress"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testGarbageReturnsFalse() {
+        let email = "#@%^%#$@#$@#.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testMissingAddressReturnsFalse() {
+        let email = "@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testEncodedHTMLWithinEmailReturnsFalse() {
+        let email = "Joe Smith <email@domain.com>"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testNoAddressWithEmaildotSubdomainReturnsFalse() {
+        let email = "email.domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testEmailWithTwoAtSymbolsReturnsFalse() {
+        let email = "email@domain@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testPeriodBeforeFirstLetterOfAddressReturnsFalse() {
+        let email = ".email@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testTrailingDotAfterAddressReturnsFalse() {
+        let email = "email.@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testTwoDotsInAddressReturnsFalse() {
+        let email = "email..email@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testUnicodeCharEmailReturnsFalse() {
+        let email = "あいうえお@domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testTextAfterEmailReturnsFalse() {
+        let email = "email@domain.com (Joe Smith)"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testMissingTopLevelDomain() {
+        let email = "email@domain"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testLeadingDashInDomainReturnsFalse() {
+        let email = "email@-domain.com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testInvalidIpAddressReturnsFalse() {
+        let email = "email@111.222.333.44444"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func testTwodotsInDomainReturnsFalse() {
+        let email = "email@domain..com"
+        let result = emailValidatorUT.isValidInput(email)
+        
+        XCTAssertFalse(result)
+    }
+}
