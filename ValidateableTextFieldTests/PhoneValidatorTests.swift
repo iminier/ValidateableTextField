13effb50734c14782f6c2f5300bf93b94695bbbb
Ivan Minier <ivanminier@me.com>
2017-07-09 20:01:28 -0700

    Adding phone tests.

---
+++ ValidateableTextFieldTests/PhoneValidatorTests.swift
@@ -0,0 +1,25 @@
+//
+//  PhoneValidatorTests.swift
+//  ValidateableTextField
+//
+//  Created by Ivan Minier on 7/9/17.
+//  Copyright © 2017 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import XCTest
+@testable import ValidateableTextField
+
+class PhoneValidatorTests: XCTestCase {
+    
+    var phoneValidatorUT = PhoneValidator()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
